6246ceeea4cac6cf67d812f7271a6a266af59172
Benjamin Babić <136199576+LighterThanAir7@users.noreply.github.com>
2024-03-17 14:36:18 +0100

    Unnecessary assets removed
    Kako radimo bg fixed

--- src/components/KakoRadimo.jsx
+++ src/components/KakoRadimo.jsx
@@ -4,10 +4,10 @@ import SectionTitle from "./SectionTitle.jsx";
 export default function KakoRadimo () {
   return (
     <section id="kako-radimo">
-      {/*<img className="hero__img" src="/src/assets/Pozadina.png" alt="Image 1"/>
+      <img className="" src="/src/assets/leaves-bg.webp" alt="leaves pattern in the background"/>
       <div className="triangle-top-right" role="presentation"></div>
       <div className="triangle-bottom-left" role="presentation"></div>
-      <div className="triangle-bottom-left-flipped-x" role="presentation"></div>*/}
+      <div className="triangle-bottom-left-flipped-x" role="presentation"></div>
       <SectionTitle title="O nama"/>
       <Steps/>
     </section>
