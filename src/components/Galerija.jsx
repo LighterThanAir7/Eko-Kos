6aa362bffa73f401ef8a713f3b79264dae666093
LighterThanAir7 <bbabic2@racunarstvo.hr>
2024-03-26 00:14:54 +0100

    Photo Gallery

--- src/components/Galerija.jsx
+++ src/components/Galerija.jsx
@@ -1,22 +1,41 @@
 import SectionTitle from "./SectionTitle.jsx";
 import galerijaCover from "/src/assets/galerija-cover.webp"
+import {useEffect, useState} from "react";
+import ImageSlider from "./ImageSlider.jsx";
 
 export default function Galerija () {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const importImages = async () => {
+      const imageModules = import.meta.glob('../assets/photo-gallery/*.webp', {eager: true});
+      const importedImages = Object.values(imageModules).map((module) => module.default);
+      setImages(importedImages);
+    };
+
+    importImages();
+  }, []);
+
   return (
-    <section id="galerija">
-      <div className="flex-basis-50">
-        <SectionTitle title="Galerija" type="fancy"/>
-        <p className="margin-bottom-20">
-          Naša galerija je mjesto gdje možete pronaći vizualnu inspiraciju za svoj  prostor. Slike su pažljivo odabrane kako bi prikazale našu stručnost u  raznim aspektima uređenja okoliša, od košnje trave i orezivanja voćaka  do estetskog uređivanja cvjetnih aranžmana.
-        </p>
-        <p className="margin-bottom-36">
-          Pregledajte naše fotografije  i otkrijte kako možemo transformirati vašu okućnicu ili vikendicu u  oazu mira i ljepote.
-        </p>
-        <button className="btn">Pokreni galeriju</button>
-      </div>
-      <div className="image-container">
-        <img src={galerijaCover} alt=""/>
-      </div>
-    </section>
+    <>
+      <section id="galerija">
+        <div className="flex-basis-50">
+          <SectionTitle title="Galerija" type="fancy"/>
+          <p className="margin-bottom-20">
+            Naša galerija je mjesto gdje možete pronaći vizualnu inspiraciju za svoj prostor. Slike su pažljivo odabrane
+            kako bi prikazale našu stručnost u raznim aspektima uređenja okoliša, od košnje trave i orezivanja voćaka do
+            estetskog uređivanja cvjetnih aranžmana.
+          </p>
+          <p className="margin-bottom-36">
+            Pregledajte naše fotografije i otkrijte kako možemo transformirati vašu okućnicu ili vikendicu u oazu mira i
+            ljepote.
+          </p>
+          <ImageSlider/>
+        </div>
+        <div className="image-container">
+          <img src={galerijaCover} alt=""/>
+        </div>
+      </section>
+    </>
   )
 }
---
+++ src/components/ImageSlider.jsx
@@ -0,0 +1,62 @@
+import {useEffect, useState} from "react";
+
+export default function ImageSlider () {
+  const [modal, setModal] = useState(false);
+
+  const toggleModal = () => {
+    setModal(!modal);
+  };
+
+  if(modal) {
+    document.body.classList.add('active-modal')
+  } else {
+    document.body.classList.remove('active-modal')
+  }
+
+
+
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const importImages = async () => {
+      const imageModules = import.meta.glob('../assets/photo-gallery/*.webp', {eager: true});
+      const importedImages = Object.values(imageModules).map((module) => module.default);
+      setImages(importedImages);
+    };
+
+    importImages();
+  }, []);
+
+  const [imageIndex, setImageIndex] = useState(0); // current index
+  const imagesLenght = images.length - 1;
+
+  function showNextImage() {
+    setImageIndex(index => {
+      if (index === imagesLenght) return 0 // loop images, get first image
+      return index + 1;
+    })
+  }
+
+  function showPreviousImage() {
+    setImageIndex(index => {
+      if (index === 0) return imagesLenght // loop images, get last image
+      return index - 1;
+    })
+  }
+
+  return (
+    <>
+      <button onClick={toggleModal} className="btn">Pokreni galeriju</button>
+
+      {modal && (
+        <div className="modal padding-image-gallery">
+          <div className="image-slider">
+            <img className="image-slider__img" src={images[imageIndex]} alt=""/>
+            <button onClick={showPreviousImage} className="image-slider__btn image-slider__btn--left"><i className="icon-arrow_left"></i></button>
+            <button onClick={showNextImage} className="image-slider__btn image-slider__btn--right"><i className="icon-arrow_left flip-x"></i></button>
+          </div>
+        </div>
+      )}
+    </>
+  )
+}
