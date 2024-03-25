d20b70d38597ec6eb1235b1b32b8e8b127bf8c9a
LighterThanAir7 <bbabic2@racunarstvo.hr>
2024-03-25 15:03:46 +0100

    Form Submission

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,16 +1,97 @@
-import Input from "./Input.jsx";
-import Textarea from "./Textarea.jsx";
+import React, {useRef, useState} from 'react';
+import * as emailjs from "@emailjs/browser";
 
-export default function Form () {
+export default function Form() {
+  const form = useRef();
+  const [formState, setFormState] = useState({
+    name: '',
+    surname: '',
+    email: '',
+    subject: '',
+    message: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormState(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const templateParams = {
+      from_name: `${formState.name} ${formState.surname}`,
+      from_email: formState.email,
+      subject: formState.subject,
+      message: formState.message,
+    };
+
+    emailjs.send('service_xwq2vop', 'template_1gd6z49', templateParams, '4ZPb-e3fZGWkJS1pw')
+      .then(
+        (result) => {
+          console.log('SUCCESS!', result.text);
+        },
+        (error) => {
+          console.log('FAILED...', error.text);
+        },
+      );
+  };
+
+
   return (
-    <form className="form flex-basis-50">
+    <form ref={form} className="form flex-basis-50" onSubmit={handleSubmit}>
       <div className="form__row">
-        <Input name="ime" placeholder="Ime" required />
-        <Input name="prezime" placeholder="Prezime" required />
+        <input
+          type="text"
+          className={`form__input ${formState.name ? 'form__input--has-text' : ''}`}
+          value={formState.name}
+          onChange={handleChange}
+          name="name"
+          placeholder="Ime"
+          required
+        />
+        <input
+          type="text"
+          className={`form__input ${formState.surname ? 'form__input--has-text' : ''}`}
+          value={formState.surname}
+          onChange={handleChange}
+          name="surname"
+          placeholder="Prezime"
+          required
+        />
       </div>
-      <Input name="predmet" placeholder="Predmet" required />
-      <Textarea name="poruka" placeholder="Poruka" cols="30" rows="10" required />
+      <input
+        type="email"
+        className={`form__input ${formState.email ? 'form__input--has-text' : ''}`}
+        value={formState.email}
+        onChange={handleChange}
+        name="email"
+        placeholder="Vaša email adresa"
+        required
+      />
+      <input
+        type="text"
+        className={`form__input ${formState.subject ? 'form__input--has-text' : ''}`}
+        value={formState.subject}
+        onChange={handleChange}
+        name="subject"
+        placeholder="Predmet"
+        required
+      />
+      <textarea
+        className={`form__input ${formState.message ? 'form__input--has-text' : ''}`}
+        value={formState.message}
+        onChange={handleChange}
+        name="message"
+        placeholder="Poruka"
+        cols="30"
+        rows="10"
+        required
+      />
       <button className="btn" type="submit">Pošalji</button>
     </form>
-  )
+  );
 }
